0b8f7fab0f3d1437649a34ff22747ae1dd89d47b
Ricky <Ricky@DESKTOP-BQLT05G>
2022-01-30 15:47:05 +0200

    Add project files.

---
+++ TzokerNumbersGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿var uniqueNumbers = new HashSet<int>();
+var uniqueTzokerNumbers = new HashSet<int>();
+var randomizer = new Random(new Random().Next());
+
+do
+{
+    var number = randomizer.Next(0, 46);
+    uniqueNumbers.Add(number);
+
+} while (uniqueNumbers.Count < 30);
+
+do
+{
+    var number = randomizer.Next(0, 21);
+    uniqueTzokerNumbers.Add(number);
+
+} while (uniqueTzokerNumbers.Count < 6);
+
+
+
+for(int i = 0; i < 6; i++)
+{
+    var numbers = new HashSet<int>();
+
+    do
+    {
+        var number = randomizer.Next(0, 30);
+        numbers.Add(uniqueNumbers.ElementAt(number));
+
+    } while (numbers.Count < 5);
+
+    var tzoker = uniqueTzokerNumbers.ElementAt(randomizer.Next(0, 6));
+
+    Console.WriteLine($"{string.Join(" - ", numbers.OrderBy(n => n))}  -  Tzoker: {tzoker}");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+Console.ReadLine();
